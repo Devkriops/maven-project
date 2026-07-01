163fb3020f35d8690308616c23e5a0ae656e188e
Devkriops <pippallakrishnarai@gmail.com>
2026-06-30 22:42:49 -0700

    Create Spring Boot Harness lab app

---
+++ src/main/java/com/harnesslab/HarnessLabApplication.java
@@ -0,0 +1,12 @@
+package com.harnesslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarnessLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarnessLabApplication.class, args);
+    }
+}
